1d2d0196c7013444b4ab614cb14e42aa7e9831bd
MACTEPyc <MACTEPyc@gmail.com>
2021-08-10 08:58:18 +0500

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  const len = Array.isArray(array) ? array.length : 0;
+  if (len < 1) {
+    return 0;
+  }
+  return array.reduce((min, item) => item < min ? item : min, array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  const len = Array.isArray(array) ? array.length : 0;
+  if (len < 1) {
+    return 0;
+  }
+  return array.reduce((max, item) => item > max ? item : max, array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  const len = Array.isArray(array) ? array.length : 0;
+  if (len < 1) {
+    return 0;
+  }
+  return (array.reduce((sum, item) => sum + item, 0) / len);
 }
